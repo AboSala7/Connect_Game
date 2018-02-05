774b4be0073387d393c1580b631167f3b2cfb612
AboSala7 <me.mohamedsala@gmail.com>
2018-02-05 19:03:11 +0200

    first updat

--- app/src/main/java/com/example/admin/connectgame/MainActivity.java
+++ app/src/main/java/com/example/admin/connectgame/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.R.attr.id;
@@ -21,50 +22,49 @@ public class MainActivity extends AppCompatActivity {
     int[][] winingPos = {{0,4,8},{2,4,6},{1,4,7},{3,4,5}};
     LinearLayout la ;
     GridLayout gl ;
+    TextView textView ;
 
-
+    int count ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        la =  (LinearLayout) findViewById(R.id.l);
+        textView = (TextView)findViewById(R.id.textView);
     }
     public void drop (View view){
 
-        ImageView counter = (ImageView) view ;
-
+        Button counter = (Button) view ;
         int place = Integer.parseInt(counter.getTag().toString());
-
 
         if((activePic == 0)&&(places[place]== 2)) {
             counter.setTranslationY(-1000f);
-            counter.setImageResource(R.drawable.yellow);
+            counter.setBackgroundResource(R.drawable.yellow);
             places[place]= activePic ;
+            count++;
             activePic = 1 ;
             counter.animate().translationYBy(1000f).rotation(720f).setDuration(500);
         }
         else if (((activePic == 1)&&(places[place]== 2))) {
             counter.setTranslationY(-1000f);
-
-            counter.setImageResource(R.drawable.red);
-            places[place]= activePic ;
+            counter.setBackgroundResource(R.drawable.red);
+            places[place]= activePic;
+            count++ ;
             activePic = 0;
             counter.animate().translationYBy(1000f).rotation(720f).setDuration(500);
         }
+
         for (int[]win : winingPos) {
             if ((places[win[0]] == places[win[1]]) && (places[win[0]] == places[win[2]]) && places[win[0]] != 2) {
-               Toast.makeText(getApplicationContext(), "there is a winner", Toast.LENGTH_LONG).show();
-                la =  (LinearLayout) findViewById(R.id.l);
+              textView.setText("Winner");
                 la.setVisibility(View.VISIBLE);
 
                 GridLayout gl = (GridLayout) findViewById(R.id.g);
                 for(int i = 0 ; i < gl.getChildCount(); i++) {
-                    ((ImageView) gl.getChildAt(i)).setClickable(false);
+                    ((Button) gl.getChildAt(i)).setClickable(false);
                 }
-
-
 
                 if (places[win[0]]==0){
                      la.setBackgroundColor(YELLOW);
@@ -74,15 +74,25 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             }
+
         }
+
+            if (count == 9) {
+                if(la.getVisibility()!= View.VISIBLE) {
+                    la.setVisibility(View.VISIBLE);
+                    textView.setText("no winner");
+                    count = 0;
+                }
+            }
+
     }
     public void tryAgain(View view){
 
-        la.setVisibility(View.INVISIBLE);
+        la.setVisibility(View.GONE);
         GridLayout gl = (GridLayout) findViewById(R.id.g);
         for(int i = 0 ; i < gl.getChildCount(); i++){
-            ((ImageView) gl.getChildAt(i)).setImageResource(0);
-            ((ImageView) gl.getChildAt(i)).setClickable(true);
+            ((Button) gl.getChildAt(i)).setBackgroundResource(R.color.white_greyish);
+            ((Button) gl.getChildAt(i)).setClickable(true);
             places[i] = 2 ;
         }
         activePic = 0 ;
